54b2ae33c839e0761bcdf2fda6c3a913def098df
201816040222 <56759713+201816040222@users.noreply.github.com>
2019-11-17 19:52:07 +0800

    Update Rectangle.cpp

--- 201816040222/Ex09_11/Rectangle.cpp
+++ 201816040222/Ex09_11/Rectangle.cpp
@@ -38,19 +38,3 @@ double Rectangle::getArea()
 {
     return  width*length ;
 }
-
-
-#ifndef RECTANGLE_H_INCLUDED
-#define RECTANGLE_H_INCLUDED
-class Rectangle
-{
-public:
-    Rectangle(double =1.0, double=1.0);
-    void setLength(double);
-    void setWidth(double);
-    double getPerimeter();
-    double getArea();
-private:
-     double length;
-     double width;
-};
