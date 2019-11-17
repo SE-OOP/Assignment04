f4dd56c2f297ba077847288d164c1b36d97d3622
小c <56589303+201816040123@users.noreply.github.com>
2019-11-17 18:41:11 +0800

    Create DeckOfCards.h
    201816040123

---
+++ 201816040123/Ex09_23/DeckOfCards.h
@@ -0,0 +1,18 @@
+#ifndef DECKOFCARDS_H_INCLUDED
+#define DECKOFCARDS_H_INCLUDED
+#include<string>
+#include<vector>
+#include<iostream>
+using namespace std;
+class DeckOfCards
+{
+public:
+	explicit DeckOfCards(const Card &);
+	void shuffle();//洗牌
+	vector <Card> getDeck();
+	void display();//展示牌组
+private:
+    vector <Card> deck;
+    int currentCard;
+};
+#endif
