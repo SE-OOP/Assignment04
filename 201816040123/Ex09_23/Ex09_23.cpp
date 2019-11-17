249f83c0aa8e834a28221f021601099331059eac
小c <56589303+201816040123@users.noreply.github.com>
2019-11-17 18:36:32 +0800

    Update Ex09_23.cpp
    201816040123

--- 201816040123/Ex09_23/Ex09_23.cpp
+++ 201816040123/Ex09_23/Ex09_23.cpp
@@ -10,7 +10,7 @@ int main()
     cout << "洗牌前:" << endl;
     b.display();//输出当前牌的顺序
     cout << endl;
-    poker.shuffle();
+    b.shuffle();//洗牌
     cout << "洗牌后:" << endl;
     b.display();//输出洗过牌后的顺序
     cout<<endl;
