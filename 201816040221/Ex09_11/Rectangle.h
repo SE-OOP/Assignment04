550da00442babe527b15a790ba4b10b084feac95
byj-999 <56639450+byj-999@users.noreply.github.com>
2019-11-17 19:20:05 +0800

    Create Rectangle.h

---
+++ 201816040221/Ex09_11/Rectangle.h
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#ifndef Rectangle_H
+#define Rectangle_H
+
+
+#include "Rectangle.h"
+
+using namespace std;
+
+class Rectangle
+
+    {
+public:
+        Rectangle(float ,float );
+        void setlength(float);
+        void setwidth(float );
+        float getlength();
+        float getwidth();
+        float getperimeter();
+        float getarea();
+
+    }//#endif
